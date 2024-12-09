93ec4119b6048e889738b0d9478c20736c855463
Furkan "Nafair <115067547+furkandlkdr@users.noreply.github.com>
2024-12-10 01:47:16 +0300

    Create Create_Faculty.sql

---
+++ Create_Faculty.sql
@@ -0,0 +1,22 @@
+USE [Course_Enrollment]
+GO
+
+/****** Object:  Table [dbo].[Faculty]    Script Date: 10.12.2024 01:47:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Faculty](
+	[Faculty_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Dean_Name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Faculty] PRIMARY KEY CLUSTERED 
+(
+	[Faculty_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
